162f6e35315971594f965a92b17710e2d8e36fe5
Iksi <jur@iksi.cc>
2015-08-25 22:47:04 +0200

    added support for 12 hour clock

--- time.php
+++ time.php
@@ -1,39 +1,40 @@
 <?php
 
-class TimeField extends InputField
-{
+class TimeField extends InputField {
 
-    public function __construct()
-    {
+    public function __construct() {
         $this->icon   = 'clock-o';
         $this->format = 24;
     }
 
-    public function format()
-    {
+    public function format() {
         return ($this->format === 12) ? 'h:i A' : 'H:i';
     }
 
-    public function value()
-    {
+    public function value() {
         if (empty($this->value) || $this->value === 'now') {
-            return date($this->format(), strtotime('now'));   
+            return date($this->format(), time());   
         }
 
-        return $this->value;
+        return date($this->format(), strtotime($this->value));
     }
 
-    public function input()
-    {
-        $input = parent::input();
+    public function maxlength() {
+        return ($this->format === 12) ? 8 : 5;
+    }
 
-        $input->attr('maxlength', 5);
+    public function input() {
+        $input = parent::input();
+        $input->attr('maxlength', $this->maxlength());
 
         return $input;
     }
 
-    public function validate()
-    {
-        return (bool) preg_match('/^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/', $this->value());
+    public function validate() {
+        $pattern = ($this->format === 12)
+            ? '/^(0[0-9]|1[0-2]):[0-5][0-9] (AM|PM)$/'
+            : '/^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/';
+
+        return preg_match($pattern, $this->value());
     }
 }
