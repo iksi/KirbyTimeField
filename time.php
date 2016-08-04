afcf421ba3cef7f5b386da8cb47a59c512b3aac6
Iksi <mail@iksi.cc>
2016-08-04 14:47:33 +0200

    full overhaul, better than ever

--- time.php
+++ time.php
@@ -2,47 +2,50 @@
 
 class TimeField extends InputField {
 
-    public $override = false;
+  public $override = false;
 
-    public function __construct() {
-        $this->icon   = 'clock-o';
-        $this->format = 24;
+  public function __construct() {
+    $this->icon = 'clock-o';
+    $this->format = 24;
+  }
+
+  public function detectFormat($value) {
+    $patterns = [
+      '/^(0?[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/',
+      '/^(0?[0-9]|1[0-2]):[0-5][0-9] ?(AM|PM)$/i'
+    ];
+
+    foreach ($patterns as $pattern) {
+      if (preg_match($pattern, $value)) return true;
     }
 
-    public function format() {
-        return ($this->format === 12) ? 'h:i A' : 'H:i';
+    return false;
+  }
+
+  public function format() {
+    return ($this->format === 24)
+      ? 'H:i'
+      : 'h:i A';
+  }
+
+  public function value() {
+    $value = $this->override()
+      ? $this->default()
+      : parent::value();
+
+    if ($value === 'now') {
+      return date($this->format($this->format), time());
     }
 
-    public function value() {
-        if ($this->override()) {
-            $value = $this->default();
-        } else {
-            $value = parent::value();
-        }
-
-        if (empty($value) || $value === 'now') {
-            return date($this->format(), time());   
-        }
-
-        return date($this->format(), strtotime($value));
+    if (empty($value) || $this->detectFormat($value) === false) {
+      return $value;
     }
 
-    public function maxlength() {
-        return ($this->format === 12) ? 8 : 5;
-    }
+    return date($this->format($this->format), strtotime($value));
+  }
 
-    public function input() {
-        $input = parent::input();
-        $input->attr('maxlength', $this->maxlength());
+  public function validate() {
+    return $this->detectFormat($this->value);
+  }
 
-        return $input;
-    }
-
-    public function validate() {
-        $pattern = ($this->format === 12)
-            ? '/^(0[0-9]|1[0-2]):[0-5][0-9] (AM|PM)$/'
-            : '/^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/';
-
-        return preg_match($pattern, $this->value());
-    }
 }
